07b5cb12b4bc4f7dc773588dea978fc57e7ed034
Sebastian Moreno Breser <sevmoreno@gmail.com>
2020-04-20 15:27:31 -0700

    Initial commit

---
+++ Favorday_functions/functions/index.js
@@ -0,0 +1,195 @@
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloPapu = functions.https.onRequest((request, response) => {
+ response.send("Hello fPaPu!");
+
+ var payload = {
+   notification: {
+     title: "New Pray Request ",
+     body: "Join us in prayer"
+   }
+ }
+
+
+ return admin.database().ref('/ChurchsFollowers/' + '/37E98093-7B60-4029-8DE2-7BD7C15840BE/' + 'ffttDqL0OWSYgfyxHrA8qKuzGtV2/').once('value', snapshot => {
+
+ //  var item = snapshot.val();
+  var name = snapshot.val();
+   // var item = snapshot.val()
+    console.log('Este es el valor'  + name);
+
+ })
+
+});
+
+
+exports.sendMensaje = functions.https.onRequest((request, response) => {
+ response.send("Hello fPaPu!");
+
+ var payload = {
+   notification: {
+     title: "Test",
+     body: " Test "
+   }
+ }
+
+ admin.messaging().sendToDevice(usuarios[property], payload)
+
+});
+
+// Devonoti
+
+exports.observeDevocional = functions.database.ref('/Devonoti/{uid}').onCreate((snapshot, context)  => {
+
+console.log('written by' + snapshot.val());
+
+var churchID = snapshot.val()
+
+return admin.database().ref('/ChurchsFollowers/' + churchID + '/').once('value', snapshot => {
+
+  var payload = {
+    notification: {
+      title: "A New devotional for you",
+      body: "We are together in the same Spirit"
+    }
+  }
+
+  // primer fecth que busca datos ... tenemos uid que es la iglesia ,,,, uid del seguidor y token del seguidor
+  const usuarios = snapshot.val();
+
+var tokensArray = ['']
+
+for (const property in usuarios) {
+  console.log(`${usuarios[property]}`);
+
+  tokensArray.push(property);
+
+admin.messaging().sendToDevice(usuarios[property], payload);
+
+}
+  return "Ok process"
+  });
+
+});
+
+exports.observeEvents = functions.database.ref('/Eventnoti/{uid}').onCreate((snapshot, context)  => {
+
+console.log('written by' + snapshot.val());
+
+var churchID = snapshot.val()
+
+return admin.database().ref('/ChurchsFollowers/' + churchID + '/').once('value', snapshot => {
+
+  var payload = {
+    notification: {
+      title: "A new event for the church",
+      body: "We are together in the same Spirit"
+    }
+  }
+
+  // primer fecth que busca datos ... tenemos uid que es la iglesia ,,,, uid del seguidor y token del seguidor
+  const usuarios = snapshot.val();
+
+var tokensArray = ['']
+
+for (const property in usuarios) {
+  console.log(`${usuarios[property]}`);
+
+  tokensArray.push(property);
+
+admin.messaging().sendToDevice(usuarios[property], payload);
+
+}
+  return "Ok process"
+  });
+
+});
+
+
+
+
+exports.observePostVersion2 = functions.database.ref('/Postnoti/{uid}').onCreate((snapshot, context)  => {
+
+console.log('written by' + snapshot.val());
+
+var churchID = snapshot.val()
+
+return admin.database().ref('/ChurchsFollowers/' + churchID + '/').once('value', snapshot => {
+
+  var payload = {
+    notification: {
+      title: "New Pray Request ",
+      body: "Join us in prayer"
+    }
+  }
+
+  // primer fecth que busca datos ... tenemos uid que es la iglesia ,,,, uid del seguidor y token del seguidor
+  const usuarios = snapshot.val();
+
+var tokensArray = ['']
+
+for (const property in usuarios) {
+  console.log(`${usuarios[property]}`);
+
+  tokensArray.push(property);
+
+admin.messaging().sendToDevice(usuarios[property], payload);
+
+}
+  return "Ok process"
+  });
+
+});
+
+
+
+
+
+
+exports.observeFollowing = functions.database.ref('/ChurchsFollowers/{uid}/{followingId}')
+  .onCreate((snapshot, context) => {
+
+    var uid = context.params.uid;
+    var followingId = context.params.followingId;
+
+    return admin.database().ref('/ChurchsFollowers/' + uid + '/' + followingId + '/').once('value', snapshot => {
+
+      // primer fecth que busca datos ... tenemos uid que es la iglesia ,,,, uid del seguidor y token del seguidor
+      var item = snapshot.val()
+      console.log('User: ' + followingId + ' is following: ' + uid + ' Este es el item'  + item);
+
+
+      // como sacamos tocken de la iglesia ----  token de la iglesia
+
+     return admin.database().ref('/Churchs/' + uid + '/' + 'fcmToken' + '/').once('value', snapshot => {
+
+       var tokenChurch = snapshot.val()
+
+
+       console.log('Este es el tocken de la church'  + tokenChurch);
+
+
+           var payload = {
+             notification: {
+               title: "A New user is following you",
+               body: " Keep adding followers  ..."
+             }
+           }
+
+        return admin.messaging().sendToDevice(tokenChurch, payload)
+
+
+
+       })
+
+
+    })
+
+  })
